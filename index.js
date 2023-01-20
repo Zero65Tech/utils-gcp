d74cd1e97e398be3a5cf5bb85c0e85c1d9e3bc23
Prashant Gupta <antshpra@gmail.com>
2023-01-20 13:52:19 +0530

    Firestore

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 exports.init = (config) => {
 
   if(config.firestore) {
-    exports.Firestore = {};
-    let Firestore = new (require('@google-cloud/firestore').Firestore)();
+
+    let firestore = require('@google-cloud/firestore');
+    exports.Firestore = {
+      FieldValue: firestore.FieldValue
+    };
+
+    let Firestore = new firestore.Firestore();
     for(let collection of config.firestore.collections)
       exports.Firestore[collection] = Firestore.collection(collection);
+
   }
 
   delete exports.init;
