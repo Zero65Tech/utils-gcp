20714e6e19a68f22d51211c4c621f69859f58972
Prashant Gupta <antshpra@gmail.com>
2023-01-24 15:48:23 +0530

    service

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 exports.init = (config) => {
 
+
   if(config.firestore) {
 
-    let firestore = require('@google-cloud/firestore');
+    const firestore = require('@google-cloud/firestore');
     exports.Firestore = {
       FieldValue: firestore.FieldValue
     };
@@ -12,6 +13,46 @@ exports.init = (config) => {
       exports.Firestore[collection] = Firestore.collection(collection);
 
   }
+
+
+  if(config.service) {
+
+    const axios = require('axios');
+    const https = require('https');
+
+    exports.Service = {};
+
+    let doGet = async (path, baseURL, params, headers) => {
+      let ret = await axios.get(path, {
+        baseURL: baseURL,
+        headers: headers,
+        params: params,
+        httpsAgent: new https.Agent({ keepAlive: true, maxSockets: Infinity }),
+      });
+      return ret.data;
+    }
+
+    Object.entries(config.service).forEach(entry => {
+
+      let service = entry[0];
+      let { baseURL, apis } = entry[1];
+
+      exports.Service[service] = {};
+
+      Object.entries(apis).forEach(entry => {
+
+        let api = entry[0];
+        let { method, path } = entry[1];
+
+        if(method == 'GET')
+          exports.Service[service][api] = async (params, headers) => await doGet(path, baseURL, params, headers);
+
+      });
+
+    });
+
+  }
+
 
   delete exports.init;
 
